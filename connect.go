216354e83a2c9d10e20db404b398ee0641bda2e6
abdismail <ismailabdulqudus063@gmail.com>
2026-05-18 09:05:24 -0500

    fixed the server hostname.

--- connect.go
+++ connect.go
@@ -21,10 +21,10 @@ func Connect() *tls.Conn {
 
 	config := &tls.Config{
 		RootCAs:    rootCAs,
-		ServerName: "127.0.0.1",
+		ServerName: "l2e-forum.local",
 	}
 
-	conn, err := tls.Dial("tcp", "127.0.0.1:443", config)
+	conn, err := tls.Dial("tcp", "l2e-forum.local:443", config)
 
 	if err != nil {
 		log.Fatalf("Unable to establish secure connection!: %v", err)
